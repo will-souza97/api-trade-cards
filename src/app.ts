87788d6645f3f9391e6003327d1d10bd066d55a3
will-souza97 <rzy.x97@gmail.com>
2021-05-22 01:37:00 -0300

    adicionando typeorm

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,8 @@ import 'dotenv/config';
 import express from 'express';
 import cors from 'cors';
 
+import './database';
+
 import authRoute from './routes/auth.route';
 
 class App {
--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -1,5 +1,10 @@
 import { Router } from 'express';
 
+import AuthController from '../controllers/auth.controller';
+
 const router = Router();
+
+router.get('/auth/steam', AuthController.redirectUrl);
+router.get('/auth/steam/authenticate', AuthController.authenticate);
 
 export default router;
