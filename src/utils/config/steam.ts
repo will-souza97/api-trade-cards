6ddda127f15b12913baf3c78dd160b3b73e9b64b
will-souza97 <rzy.x97@gmail.com>
2021-05-25 02:25:18 -0300

    fazendo integagrcao com o banco de dados postegres

--- src/utils/config/steam.ts
+++ src/utils/config/steam.ts
@@ -1,10 +1,11 @@
 // @ts-ignore-
 import SteamAuth from 'blaz-steam-api';
 
-// const steam = new SteamAuth({
+const steam = [];
+// new SteamAuth({
 //   realm: process.env.BASE_URL,
 //   returnUrl: `${process.env.BASE_URL}/auth/steam/authenticate`,
 //   apiKey: process.env.API_KEY,
 // });
 
-// export default steam;
+export default steam;
