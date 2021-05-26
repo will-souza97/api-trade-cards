1d82651ad85dea3670b6942a53baae2297b9ed54
will-souza97 <rzy.x97@gmail.com>
2021-05-26 01:23:34 -0300

    removendo postgres e adicionando sqlite

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,10 +1,3 @@
 import { createConnection } from 'typeorm';
 
-createConnection({
-  type: 'postgres',
-  port: 5432,
-  host: process.env.PG_HOST,
-  username: process.env.PG_USER,
-  password: process.env.PG_PASSWORD,
-  database: process.env.PG_DATABASE,
-});
+createConnection();
